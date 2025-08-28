ffa04ce5367ea12ff1187538a1f9390420fe3115
Przekazmierczak <prze.kazmierczak@gmail.com>
2025-08-28 21:12:48 +0200

    Update leetcode 174, 181, 183, 236

---
+++ leetcode/0174_Dungeon_Game.cs
@@ -0,0 +1,58 @@
+/*
+The demons had captured the princess and imprisoned her in the bottom-right corner of a dungeon. The dungeon consists of m x n rooms laid out in a 2D grid. Our valiant knight was initially positioned in the top-left room and must fight his way through dungeon to rescue the princess.
+The knight has an initial health point represented by a positive integer. If at any point his health point drops to 0 or below, he dies immediately.
+Some of the rooms are guarded by demons (represented by negative integers), so the knight loses health upon entering these rooms; other rooms are either empty (represented as 0) or contain magic orbs that increase the knight's health (represented by positive integers).
+To reach the princess as quickly as possible, the knight decides to move only rightward or downward in each step.
+Return the knight's minimum initial health so that he can rescue the princess.
+Note that any room can contain threats or power-ups, even the first room the knight enters and the bottom-right room where the princess is imprisoned.
+
+Example 1:
+Input: dungeon = [[-2,-3,3],[-5,-10,1],[10,30,-5]]
+Output: 7
+Explanation: The initial health of the knight must be at least 7 if he follows the optimal path: RIGHT-> RIGHT -> DOWN -> DOWN.
+
+Example 2:
+Input: dungeon = [[0]]
+Output: 1
+
+Constraints:
+m == dungeon.length
+n == dungeon[i].length
+1 <= m, n <= 200
+-1000 <= dungeon[i][j] <= 1000
+
+*/
+
+public class Solution
+{
+    public int CalculateMinimumHP(int[][] dungeon)
+    {
+        int ROWS = dungeon.Length;
+        int COLS = dungeon[0].Length;
+
+        for (int row = ROWS - 1; row >= 0; row--)
+        {
+            for (int col = COLS - 1; col >= 0; col--)
+            {
+                if (row + 1 != ROWS || col + 1 != COLS)
+                {
+                    if (row + 1 == ROWS)
+                    {
+                        dungeon[row][col] += dungeon[row][col + 1];
+                    }
+                    else if (col + 1 == COLS)
+                    {
+                        dungeon[row][col] += dungeon[row + 1][col];
+                    }
+                    else
+                    {
+                        dungeon[row][col] += Math.Max(dungeon[row + 1][col], dungeon[row][col + 1]);
+                    }
+                }
+                dungeon[row][col] = Math.Min(0, dungeon[row][col]);
+            }
+        }
+
+        return Math.Abs(dungeon[0][0]) + 1;
+    }
+}
