28d0a2d1ef0d79ba611940ae588726444835cdac
Przemysław Kaźmierczak <przemyslawkazmierczak@Przemysaws-MacBook-Air.local>
2026-02-13 12:02:51 +0100

    Create token.cpp to move operator<< function

---
+++ my_language/token.cpp
@@ -0,0 +1,13 @@
+#include "token.h"
+
+std::ostream& operator << (std::ostream& cout, const Token& token)
+{
+    if (token.type == Token::Type::Int) {
+        cout << "(" << names[static_cast<int>(token.type)] << ", " << token.val << ", [" << token.line << ", " << token.position << "])";
+    } else if (token.type == Token::Type::Var) {
+        cout << "(" << names[static_cast<int>(token.type)] << ", \"" << token.name << "\", [" << token.line << ", " << token.position << "])";
+    } else {
+        cout << "(" << names[static_cast<int>(token.type)] << ", [" << token.line << ", " << token.position << "])";
+    }
+    return cout;
+}
